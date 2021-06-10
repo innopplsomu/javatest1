c383e9c78185a025692e8def081d6e3c91f757e1
innopplsomu <85595915+innopplsomu@users.noreply.github.com>
2021-06-10 15:14:26 +0530

    livecheck
    this is live check

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("Hello Agni");
-  System.out.println("somu success");
+    System.out.println("Hello Test");
+  System.out.println("live check success");
 
  }
 
