9b2f216a8375f8bb0cfb61488266621388f6d4f4
innopplsomu <85595915+innopplsomu@users.noreply.github.com>
2021-06-10 15:16:14 +0530

    errorcheck
    this is error check

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Hello Test");
-  System.out.println("live check success");
+  System.out.println("live check success")
 
- }
+ 
 
 }
